5244bd2cf24e0ffab8e2b6138c9f7c0ed0b42227
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-01 18:23:45 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 굳이 String 변수를 생성하지 않고 입력과 동시에 구분자로 분리해줘도 된다.
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		BigInteger totalMoney = new BigInteger(st.nextToken());
+		BigInteger numberOfLife = new BigInteger(st.nextToken());
+		
+		System.out.println(totalMoney.divide(numberOfLife));
+		System.out.println(totalMoney.remainder(numberOfLife));
+		
+		/* 다른 풀이 
+		 
+		 */
+	
+	}
+}
