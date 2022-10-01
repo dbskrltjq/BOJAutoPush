b1d7ff80d87bbc1bfb3b7da3ee39426b58c2e6d5
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-01 16:40:03 +0900

    Update A－B.java

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -14,5 +14,16 @@ public class Main {
 		
 		System.out.println(A - B);
 	
+		
+		// 다른 방법
+		/* 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A-B);
+ 
+		in.close();
+		*/
 	}
 }
