cb8c761ccfa5945e35a68fb3a7d82b5273cb4667
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-01 16:41:35 +0900

    Update A＋B.java

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -10,9 +10,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str, " ");
 		
-		int A = Integer.parseInt(st.nextToken());
+		// 문자열 분리 방법 두 가지
+		// 1. StringTokenizer 클래스를 이용하여 분리해주는 방법
+		// 2. split() 을 이용하는 방법
+		StringTokenizer st = new StringTokenizer(str, " ");	
+		
+		int A = Integer.parseInt(st.nextToken());	// st.nextToken() 은 문자열을 반환하니 Integer.parseInt()로 int 형으로 변환시켜준다.
 		int B = Integer.parseInt(st.nextToken());
 		
 		System.out.println(A + B);
