a63fc82f91098a5738a974d9bc031bab561be4b4
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-03 13:26:10 +0900

    Update 별 찍기 － 1.java

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -19,8 +19,20 @@ public class Main {
 		
 	}
 	
-	// 다른 방법
 	/*
+	BufferedWriter 
+	- 한번에 모았다가 출력이 가능하다.
+	- System.out.println() 처럼 자동개행 기능이 없기 때문에 개행이 필요한 경우 \n 을 통해 따로 처리해 주어야한다.
+	
+	 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //선언
+	  String sr = "abcdefg"; 	//출력할 문자열
+	  bw.write(sr); 		//출력
+	  bw.newLine();			//줄바꿈
+	  bw.flush(); 			//남아있는 데이터를 모두 출력시킴
+	  bw.close(); 			//스트림을 닫음
+	
+	*/
+	/* 다른 방법
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -29,12 +41,12 @@ public class Main {
  
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= i; j++) {
-				bw.write('*');
+				bw.write('*');		// write("출력할 내용") 으로 출력할 내용을 담는다.
 			}
 			bw.newLine();
 		}
-		bw.flush();
-		bw.close();
+		bw.flush();				// write로 담은 내용 출력 후, 버퍼를 비운다.
+		bw.close();				// 스트림 닫기
 	}
 	*/
 }
