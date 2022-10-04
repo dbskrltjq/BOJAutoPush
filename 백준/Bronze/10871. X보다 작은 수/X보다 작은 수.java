6e9989311f8db56264b0423e81e9ee8297658275
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-04 23:24:00 +0900

    [Bronze V] Title: X보다 작은 수, Time: 204 ms, Memory: 16724 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = scan.nextInt();		// 주어진 정수의 개수
-		int X = scan.nextInt();		// 비교 기준 정수
+		int N = Integer.parseInt(st.nextToken());		// 주어진 정수의 개수
+		int X = Integer.parseInt(st.nextToken());		// 비교 기준 정수
+		st = new StringTokenizer(br.readLine());		// 정수 N개가 있는 수열 A
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < N; i++) {
-			int A = scan.nextInt();
+			int a = Integer.parseInt(st.nextToken());
 			
-			if(A < X) {
-				System.out.print(A + " ");
+			if(a < X) {
+				sb.append(a + " ");
 			}
 		}
+		System.out.println(sb);
 	}
 }
