9cadccf5fee7d3571731c2ed5d4a6e1bd95384f9
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-07 17:51:02 +0900

    Update 모음의 개수.java

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -12,7 +12,7 @@ public class Main {
 		while(true) {
 			String inputData = bf.readLine();
 			
-			if("#".equals(inputData)) {
+			if("#".equals(inputData)) {			// 사용자가 "#"를 입력하면 무한반복문을 빠져나온다.
 				break;
 			}
 			
@@ -28,7 +28,7 @@ public class Main {
 		System.out.println(sb);
 	}
 	
-	public static boolean isVowels(char oneData) {
+	public static boolean isVowels(char oneData) {			// 소문자 모음에 해당하는 아스키코드와 일치하면 true, 아니면 false를 반환
 		
 		char alphabet = Character.toLowerCase(oneData);
 		boolean result = false;
@@ -59,3 +59,56 @@ public class Main {
 		
 	}
 }
+
+
+// 다른 방법
+// 정적 메소드를 따로 만들어서 사용해보고 싶어서 위에 처럼 코드를 작성했더니 뭔가 더 코드가 길어져서 그런가 복잡해보이는건..
+/*
+public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	StringBuilder sb = new StringBuilder();
+	
+        while (true) {
+           String inputData = bf.readLine();
+
+            if (inputData.equals("#")) {
+                break;
+            }
+
+            char[] index = {'A','E','I','O','U','a','e','i','o','u'};			// 모음 대소문자
+            int vowelsCount = 0;
+
+            for (int i = 0; i < inputData.length(); i++) {
+                char c = inputData.charAt(i);
+                for (int j = 0; j < index.length; j++) {
+                    if(c == index[j]) {
+                        vowelsCount++;
+                    }
+                }
+		sb.append(vowelsCount + "\n");
+            }
+
+            System.out.println(sb);
+        }
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
