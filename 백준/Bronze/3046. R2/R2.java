1d520dc72ac0e0ab653004f62c226e072051ad36
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-07 22:56:11 +0900

    [Bronze IV] Title: R2, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException  {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+		
+		int r1 = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		System.out.println(2*s - r1); 
+    }
+}
