8b66a2281e46128a9d8a4c97ae4932fa666d3bc2
youna <107186827+dbskrltjq@users.noreply.github.com>
2022-10-08 10:23:36 +0900

    Update 방학 숙제.java

--- 백준/Bronze/5532. 방학 숙제/방학 숙제.java
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -9,18 +9,25 @@ public class Main {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
 		int L = Integer.parseInt(bf.readLine());				// 방학일 수
-		int A = Integer.parseInt(bf.readLine());				// 풀어야하는 국어 페이지
-		int B = Integer.parseInt(bf.readLine());				// 수학 페이지
-		int C = Integer.parseInt(bf.readLine());				// 하루에 풀 수 있는 국어 페이지
-		int D = Integer.parseInt(bf.readLine());				// 하루에 풀 수 있는 수학 페이지
+		double A = Integer.parseInt(bf.readLine());				// 풀어야하는 국어 페이지
+		double B = Integer.parseInt(bf.readLine());				// 수학 페이지
+		double C = Integer.parseInt(bf.readLine());				// 하루에 풀 수 있는 국어 페이지
+		double D = Integer.parseInt(bf.readLine());				// 하루에 풀 수 있는 수학 페이지
 		
-		int a = A/C;
-		int b = B/D;
+		double a = A/C;
+		double b = B/D;
 		
-		if(a >= b) {
-			System.out.println(A%C == 0 ? L - a : L - a - 1);
-		} else {
-			System.out.println(B%D == 0 ? L - b : L - b - 1);
-		}
+		int homeworkDays = (int)Math.ceil(bigger(a, b));
+		
+		System.out.println(L - homeworkDays);
+		
     }
+	
+	public static double bigger(double a, double b) {
+		if(a > b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
 }
